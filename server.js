e217812eb299fded6df25b1a2c05adf9b5d64c83
Hernán Baravalle <hernan.baravalle@gmail.com>
2024-10-03 22:03:21 -0300

    cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import 'dotenv/config';
+import cors from 'cors';
 import connectDB from './config/mongoose.config.js';
 import express from 'express';
 import recipeRoutes from './routes/recipeRoutes.js';
 const app = express();
+
+app.use(cors());
 
 connectDB();
 app.use(express.json());
